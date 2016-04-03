00ac9f6fa0a3f61ef269ac6f88efe928bb708db0
katschre <schreiber.katrina@gmail.com>
2016-04-03 16:42:12 +0000

    showing photos hopefully

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ gem 'sass-rails'
 gem 'devise', '>= 3.2.4'
 gem 'geocoder'
 gem "figaro", ">= 1.0.0"
+gem 'carrierwave'
 
 
 gem 'rails_12factor', group: :production
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160403154726) do
+ActiveRecord::Schema.define(version: 20160403163109) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20160403154726) do
     t.integer  "place_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "avatar"
   end
 
   add_index "photos", ["place_id"], name: "index_photos_on_place_id", using: :btree
